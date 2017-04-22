a9b7f89e56762f3e1b02fb20462e895e723e64c3
Denis Yuzhakov <den.yuzhakov@gmail.com>
2017-04-22 14:55:00 +0200

    edited db credentials

--- webservice/db/credentials.js
+++ webservice/db/credentials.js
@@ -3,12 +3,12 @@
 	
 	//noinspection UnnecessaryLocalVariableJS
 	const credentials = {
-		host: "sql.freemysqlhosting.net",
+		host: "",
 		port: "3306",
 		connectionLimit: 10,
-		user: "sql8169061",
-		password: "FAIw4aN86I",
-		database: "sql8169061"
+		user: "",
+		password: "",
+		database: ""
 	};
 	
 	module.exports = credentials;
