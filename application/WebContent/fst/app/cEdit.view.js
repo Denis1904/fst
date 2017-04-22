a6ae86105bf764bb26e0d883359d02aebb171083
Denis Yuzhakov <den.yuzhakov@gmail.com>
2017-04-22 16:09:20 +0200

    Merge remote-tracking branch 'origin/master'

--- application/WebContent/fst/app/cEdit.view.js
+++ application/WebContent/fst/app/cEdit.view.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 (function () {
 	"use strict";
 
@@ -28,4 +29,36 @@
 			return page;
 		}
 	});
+=======
+(function () {
+	"use strict";
+
+	sap.ui.jsview("fst.app.cEdit", {
+
+		getControllerName: function () {
+			return "fst.app.cEdit";
+		},
+
+		createContent: function (oController) {
+
+			//Back button
+			var oBackButton = new sap.m.Button({
+				text: "Zurück",
+				icon: "sap-icon://arrow-left",
+				press: oController.handleBackBtnPress
+			});
+
+
+			var page = new sap.m.Page({
+
+				title: "Vertragspflege",
+				enableScrolling: false,
+				content: [oBackButton]
+
+			});
+
+			return page;
+		}
+	});
+>>>>>>> origin/master
 })();
