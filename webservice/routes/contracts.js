7054f94bbd353ac4945ae3b7c5982e5947de92a1
Sascha Bauer <post@saschabauer.eu>
2017-05-01 20:00:06 +0200

    added route for status change of contracts

--- webservice/routes/contracts.js
+++ webservice/routes/contracts.js
@@ -8,38 +8,48 @@
 	
 	let sRoute = "/contracts";
 	app.get(sRoute, bJson, function(req, res, next) {
-		logger.info("Hit route " + sRoute + " Mode: GET");
+		logger.info("Hit route /contracts Mode: GET");
 		res.set("Content-Type", "application/javascript");
-
+		
 		queries.getContracts().then(oResp => {
 				res.send(oResp);
 			}
 		);
-
+		
 	});
 	
 	sRoute = "/getAllowedStatus";
 	app.post(sRoute, bJson, function(req, res, next) {
-		logger.info("Hit route " + sRoute + " Mode: POST");
+		logger.info("Hit route /getAllowedStatus Mode: POST");
 		res.set("Content-Type", "application/javascript");
 		
 		const oContract = req.body;
 		const oCntr = new ContractController();
 		res.send(oCntr.getAllowedStatus(oContract.id));
+	});
+	
+	sRoute = "/changeContractStatus";
+	app.post(sRoute, bJson, function(req, res, next) {
+		logger.info("Hit route /changeContractStatus Mode: POST");
+		res.set("Content-Type", "application/javascript");
+		
+		const oContract = req.body;
+		const oCntr = new ContractController();
+		oCntr.changeContractStatus(oContract.id, oContract.status).then(aReturn => res.send(aReturn));
 		
 	});
-
+	
 	sRoute = "/addContract";
-	app.post(sRoute, bJson, function (req, res, next) {
-		logger.info("Hit route " + sRoute + " Mode: POST");
+	app.post(sRoute, bJson, function(req, res, next) {
+		logger.info("Hit route /addContract Mode: POST");
 		res.set("Content-Type", "application/javascript");
 		const oContract = req.body;
-
+		
 		queries.addContracts(oContract).then(oResp => {
 				res.send(oResp);
 			}
 		);
-
+		
 	});
-
+	
 })();
