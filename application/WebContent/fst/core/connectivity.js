1e967a390654b303713285330a20c75d2bca1d51
Sascha Bauer <post@saschabauer.eu>
2017-05-25 22:51:50 +0200

    - changed url to proxy location - added user webservice - changed loading of data from json model request to simple ajax request

--- application/WebContent/fst/core/connectivity.js
+++ application/WebContent/fst/core/connectivity.js
@@ -5,34 +5,51 @@
 	
 	fst.core.connectivity = function() {};
 	
+	fst.core.connectivity.prototype.__btoa = undefined;
+	
 	fst.core.connectivity.prototype.__oModel = undefined;
 	
-	fst.core.connectivity.prototype.__serviceUrl = "http://localhost:3000/";
+	fst.core.connectivity.prototype.__serviceUrl = "/proxy/";
 	
 	fst.core.connectivity.prototype.__loadData = function(sUrl, sLoadingText, oOptions) {
 		const sType = _.get(oOptions, "sType", "GET");
 		const oBody = _.get(oOptions, "oBody", null);
+		const sBtoa = _.get(this, "__btoa", sessionStorage.getItem("HASH"));
 		return new Promise((fnResolve, fnReject) => {
 			this.__BusyDialog = new sap.m.BusyDialog({
 				title: oBundle.getText("std.isLoading"),
 				text: sLoadingText || oBundle.getText("std.pleaseWait")
 			});
-			this.__oModel = new sap.ui.model.json.JSONModel();
-			this.__oModel.attachRequestSent(() => {
-				this.__BusyDialog.open();
-			});
-			this.__oModel.attachRequestCompleted(null, (oRequestDetails) => {
-				if (oRequestDetails.getParameter("success")) {
+			
+			jQuery.ajax({
+				type: sType,
+				contentType: "application/json",
+				url: _.join([this.__serviceUrl, sUrl], ""),
+				headers: { Authorization: "Basic " + sBtoa },
+				dataType: "json",
+				data: JSON.stringify(oBody),
+				beforeSend: oJqXHR => {
+					this.__BusyDialog.open();
+				},
+				success: (oData, sTextStatus, oJqXHR) => {
 					this.__BusyDialog.close();
-					fnResolve(this.__oModel.getData());
+					fnResolve(oData);
+				},
+				error: (oJqXHR, sTextStatus, oErrorThrown) => {
+					this.__BusyDialog.close();
+					fnReject(oJqXHR);
 				}
 			});
-			this.__oModel.attachRequestFailed(null, (oData) => {
-				this.__BusyDialog.close();
-				fnReject(oData);
-			});
-			sUrl = _.join([this.__serviceUrl, sUrl], "");
-			this.__oModel.loadData(sUrl, oBody, false, sType, false, false);
+			
+		});
+	};
+	
+	fst.core.connectivity.prototype.getUser = function(sUser) {
+		return this.__loadData("getUser", oBundle.getText("contract.loading.multiple"), {
+			sType: "POST",
+			oBody: {
+				sUser: sUser
+			}
 		});
 	};
 	
@@ -57,7 +74,10 @@
 	fst.core.connectivity.prototype.changeContractStatus = function(sContractId, sNewStatus) {
 		return this.__loadData("changeContractStatus", oBundle.getText("contract.loading.multiple"), {
 			sType: "POST",
-			oBody: { id: sContractId, status: sNewStatus }
+			oBody: {
+				id: sContractId,
+				status: sNewStatus
+			}
 		});
 	};
 	
